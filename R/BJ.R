7caea2b8ff1cb554ab4b13e86f9ef3b773f3d514
Ryan Sun <ryanrsun@gmail.com>
2016-10-10 14:19:10 -0400

    Check for Z>8.2 at beginning now

--- R/BJ.R
+++ R/BJ.R
@@ -1,92 +1,96 @@
 #' BJ.R
 #'
-#' Given a vector of individual test statistics and their pairwise correlations, calculate 
+#' Given a vector of individual test statistics and their pairwise correlations, calculate
 #' the Berk-Jones second-level test statistic and it's p-value.
 #'
 #' @param test_stats Vector of all individual (first-level) test statistics
-#' @param pairwise_cors A vector of all d(d-1)/2 pairwise correlations between the test 
-#' statistics, where d is total number of test statistics in the set.  
+#' @param pairwise_cors A vector of all d(d-1)/2 pairwise correlations between the test
+#' statistics, where d is total number of test statistics in the set.
 #'
 #' @return A list with the elements:
 #' \item{BJ}{The observed Berk-Jones test statistic.}
-#' \item{BJ_pvalue}{The p-value of this observed value, given the size of the set and 
+#' \item{BJ_pvalue}{The p-value of this observed value, given the size of the set and
 #' correlation structure.}
 #'
 #' @export
-#' @examples 
+#' @examples
 #' # Should return statistic = 1.243353 and p_value = 0.256618
 #' set.seed(100)
 #' test_stats <- rnorm(5) + rep(1,5)
 #' BJ(test_stats, pairwise_cors=rep(0.2,10))
 
 BJ <- function(test_stats, pairwise_cors) {
-	
+
 	# Ensure that the thresholds are sorted in descending order, largest first.
 	t_vec <- sort(abs(test_stats), decreasing=TRUE)
 	d <- length(t_vec)
+
+	# Sometimes test stats are too big for R's precision
+	too_big <- which(t_vec > 8.2)
+	if (length(too_big) > 0) {t_vec[too_big] <- 8.2}
 
 	# Correct number of pairwise correlations?
 	if (length(pairwise_cors) != d*(d-1)/2) {
 		stop("Your pairwise correlation vector is of the wrong length!")
 	}
-	
+
 	# Check for qualifying p-values under the null (the indicator part of the GBJ statistic)
 	# and also that we are only considering 'first half' p-values
 	p_values <- 1-pchisq(t_vec^2, df=1)
 	BJ_indicator <- which( p_values < (1:d)/d )
 	first_half <- 1:(ceiling(d/2))
 	non_zero <- intersect(BJ_indicator, first_half)
-	
+
 	# If no indicies qualified, stop
 	if (length(non_zero) == 0) {
 		return ( list(BJ=0, BJ_pvalue=1) )
 	}
-	
+
 	#################################
 	# Some indicies passed
 	i_vec <- 1:d
 	BJ_stats <- rep(0, d)
-	BJ_stats[non_zero] <- i_vec[non_zero] * log(i_vec[non_zero]/(d*p_values[non_zero])) + 
-								(d-i_vec[non_zero]) * log((1-i_vec[non_zero]/d)/(1-p_values[non_zero]))	
+	BJ_stats[non_zero] <- i_vec[non_zero] * log(i_vec[non_zero]/(d*p_values[non_zero])) +
+								(d-i_vec[non_zero]) * log((1-i_vec[non_zero]/d)/(1-p_values[non_zero]))
 	BJ_stats[d] <- 0
-	
+
 	# Observed BJ statistic
 	b <- max(BJ_stats[1:(ceiling(d/2))])
-	
+
 	# Calculate p-value
 	if (b<=0) {
 		return ( list(BJ=0, BJ_pvalue=1) )
 	}
-	
+
 	# BJ bounds
 	BJ_p_bounds <- rep(NA, d)
-		
+
 	# Use uniroot to find the pvalue bounds.
 	for ( jjj in 1:(ceiling(d/2)) ) {
-		BJ_p_bounds[jjj] <- uniroot(f=function(x, k, d, b) {k*log(k/(d*x)) + 
+		BJ_p_bounds[jjj] <- uniroot(f=function(x, k, d, b) {k*log(k/(d*x)) +
 										(d-k)*log((1-k/d)/(1-x)) - b}, k=jjj, d=d,
 										b=b, lower=0, upper=jjj/d, tol=(10^(-12)))$root
-	} 
-				
+	}
+
 	# The last half of the order statistic bounds
 	BJ_p_bounds[(ceiling(d/2)+1):d] <- BJ_p_bounds[ceiling(d/2)]
-	
+
 	# Now put the bounds in terms of the Z statistics
 	BJ_z_bounds <- qnorm(1 - BJ_p_bounds/2)
 	BJ_z_bounds <- sort(BJ_z_bounds, decreasing=F)
-	
+
 	# qnorm can't handle more precision than 10^-16
 	# Also crossprob_cor can only handle Z up to 8.2
 	BJ_z_bounds[which(BJ_z_bounds > 8.2)]= 8.2
-	
+
 	# Send it to the C++.
 	if (sum(abs(pairwise_cors)) == 0) {
 		# For the independence flag in the c++, just have to send a number < -1.
-		BJ_corp <- ebb_crossprob_cor_R(d=d, bounds=BJ_z_bounds, correlations=rep(-999,2))	
+		BJ_corp <- ebb_crossprob_cor_R(d=d, bounds=BJ_z_bounds, correlations=rep(-999,2))
 	} else {
 		BJ_corp <- ebb_crossprob_cor_R(d=d, bounds=BJ_z_bounds, correlations=pairwise_cors)
 	}
-		
-	
-	return ( list(BJ=b, BJ_pvalue=BJ_corp) )	
+
+
+	return ( list(BJ=b, BJ_pvalue=BJ_corp) )
 }
--- R/HC.R
+++ R/HC.R
@@ -1,30 +1,34 @@
 #' HC.R
 #'
-#' Given a vector of individual test statistics and their pairwise correlations, calculate 
+#' Given a vector of individual test statistics and their pairwise correlations, calculate
 #' the Higher Criticism second-level test statistic and it's p-value.
 #'
 #' @param test_stats A scalar or vector of threshold points (magnitudes of test statistics)
-#' @param pairwise_cors A vector of all d(d-1)/2 pairwise correlations between the test 
-#' statistics, where d is total number of test statistics in the set.  
+#' @param pairwise_cors A vector of all d(d-1)/2 pairwise correlations between the test
+#' statistics, where d is total number of test statistics in the set.
 #'
 #' @return A list with the elements:
 #' \item{HC}{The observed Higher Criticism test statistic.}
-#' \item{HC_pvalue}{The p-value of this observed value, given the size of the set and 
+#' \item{HC_pvalue}{The p-value of this observed value, given the size of the set and
 #' correlation structure.}
 #'
 #' @export
-#' @examples 
+#' @examples
 #' # Should return statistic = 2.067475 and p_value = 0.2755146
 #' set.seed(100)
 #' test_stats = rnorm(5) + rep(1,5)
 #' HC(test_stats, pairwise_cors=rep(0.2,10))
 
 HC <- function(test_stats, pairwise_cors) {
-	
+
 	# Ensure that the thresholds are sorted in descending order, largest first.
 	t_vec <- sort(abs(test_stats), decreasing=TRUE)
 	d <- length(t_vec)
-	
+
+	# Sometimes test stats are too big for R's precision
+	too_big <- which(t_vec > 8.2)
+	if (length(too_big) > 0) {t_vec[too_big] <- 8.2}
+
 	# Correct number of pairwise correlations?
 	if (length(pairwise_cors) != d*(d-1)/2) {
 		stop("Your pairwise correlation vector is of the wrong length!")
@@ -32,37 +36,37 @@ HC <- function(test_stats, pairwise_cors) {
 
 	# Calculate HC objectives
 	p_values <- 1-pchisq(t_vec^2, df=1)
-	i_vec <- 1:d	
+	i_vec <- 1:d
 	HC_stats <- sqrt(d) * (i_vec/d - p_values) / sqrt(p_values*(1-p_values))
 
 	# Observed HC statistic
 	h <- max(HC_stats)
-	
+
 	# Calculate p-value
 	if (h<=0) {
 		return ( list(HC=0, HC_pvalue=1) )
 	}
-	
+
 	# BJ bounds
 	HC_p_bounds <- rep(NA, d)
-		
+
 	# Explicit inverse of HC to find the p-value bounds
 	HC_p_bounds <- ((2*i_vec+h^2)/d - sqrt((2*i_vec/d+h^2/d)^2 - 4*i_vec^2/d^2 - 4*i_vec^2*h^2/d^3))/(2*(1+h^2/d))
 	HC_z_bounds <- qnorm(1-HC_p_bounds/2)
 	HC_z_bounds <- sort(HC_z_bounds, decreasing=F)
-		
+
 	# qnorm can't handle more precision than 10^-16
 	# Also crossprob_cor can only handle Z up to 8.2
 	HC_z_bounds[which(HC_z_bounds > 8.2)]= 8.2
-	
+
 	# Send it to the C++.
 	if (sum(abs(pairwise_cors)) == 0) {
 		# For the independence flag in the c++, just have to send a number < -1.
-		HC_corp <- ebb_crossprob_cor_R(d=d, bounds=HC_z_bounds, correlations=rep(-999,2))	
+		HC_corp <- ebb_crossprob_cor_R(d=d, bounds=HC_z_bounds, correlations=rep(-999,2))
 	} else {
 		HC_corp <- ebb_crossprob_cor_R(d=d, bounds=HC_z_bounds, correlations=pairwise_cors)
 	}
-	
-	
-	return ( list(HC=h, HC_pvalue=HC_corp) )	
+
+
+	return ( list(HC=h, HC_pvalue=HC_corp) )
 }
