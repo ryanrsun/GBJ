464e5f43995b2cc598c31969eaef0cc2d7f53d87
Ryan Sun <ryanrsun@gmail.com>
2016-10-18 18:51:05 -0400

    Add poisson regression

--- R/calc_score_stats.R
+++ R/calc_score_stats.R
@@ -1,70 +1,72 @@
 #' calc_score_stats.R
 #'
 #' Starting with individual-level data on p factors, generate score test statistics for each
-#' factor for input into GBJ/GHC/HC/BJ/minP.  Also get the correlations between these test statistics. 
+#' factor for input into GBJ/GHC/HC/BJ/minP.  Also get the correlations between these test statistics.
 #' Designed to be used with linear or logistic regression null models.
 #'
 #' @param null_model A fitted R regression model
 #' @param factor_matrix An n*p matrix with each factor as one column.  There should be no missing data.
-#' @param model_type Either "linear" or "logistic"
+#' @param link_function Either "linear" or "logistic" or "log"
 #'
 #' @return A list with the elements:
 #' \item{test_stats}{The p score test statistics.}
 #' \item{cor_mat}{The p*p matrix giving the pairwise correlation of every two test statistics.}
 #'
 #' @export
-#' @examples 
+#' @examples
 #' Y <- rbinom(n=100, size=1, prob=0.5)
 #' null_mod <- glm(Y~1, family=binomial(link="logit"))
 #' factor_mat <- matrix(data=rnorm(n=100*5), nrow=100)
 #' calc_score_stats(null_mod, factor_mat, "logistic")
 
-calc_score_stats <- function(null_model, factor_matrix, model_type) {
-	
+calc_score_stats <- function(null_model, factor_matrix, link_function) {
+
 	X_mat <- model.matrix(null_model)
 	fitted_Y <- null_model$fitted.values
 	actual_Y <- null_model$y
-	
+
 	# Only difference between linear and logistic procedure
-	if (model_type == 'logistic') {
+	if (link_function == 'logistic') {
 		W_vec <- fitted_Y * (1-fitted_Y)
-	} else if (model_type == 'linear') {
+	} else if (link_function == 'linear') {
 		W_vec <- rep(summary(null_model)$sigma^2, nrow(X_mat))
+	} else if (link_function == 'log') {
+		W_vec <- fitted_Y
 	} else {
 		stop("Invalid model type")
 	}
-	
+
 	########################
 	# EZ Mode if linear regression, no additional covariates except for intercept
-	if (model_type == 'linear' & formula(null_model) == "Y ~ 1") {
+	if (link_function == 'linear' & formula(null_model) == "Y ~ 1") {
 		num_sub <- nrow(X_mat)
 		sig_sq_hat <- sum( (actual_Y - fitted_Y)^2 ) / (num_sub-1)
-		test_stats <- rep(NA, d) 
+		test_stats <- rep(NA, d)
 		denominators <- rep(NA, d)
-		for(kkk in 1:d) 
+		for(kkk in 1:d)
 		{
 			tempF<- factor_matrix[,kkk]
-			score_num <- t(tempF) %*% (actual_Y-fitted_Y) 
+			score_num <- t(tempF) %*% (actual_Y-fitted_Y)
 			score_denom <- sqrt(sig_sq_hat * (sum(tempF^2) - mean(tempF)^2*num_sub))
 			denominators[kkk] <- score_denom
 			test_stats[kkk] <- score_num / score_denom
 		}
 		est_cor <- cor(factor_matrix)
-		
+
 		# Return from here
-		return ( list(test_stats=test_stats, cor_mat=est_cor) )	
+		return ( list(test_stats=test_stats, cor_mat=est_cor) )
 	}
-	
+
 	########################
 	# Regular mode
 	W_mat <- diag(W_vec)
 	P_mat <- W_mat - W_mat%*%X_mat %*% solve(t(X_mat)%*%W_mat%*%X_mat) %*% t(X_mat)%*%W_mat
-	
+
 	# Now our score test
 	d <- ncol(factor_matrix)
 	test_stats <- rep(NA, d)
 	denominators <- rep(NA, d)
-	for(kkk in 1:d) 
+	for(kkk in 1:d)
 	{
 		# Pick out next SNP, conduct score test (no additional covariates).
 		tempF <- factor_matrix[,kkk]
@@ -85,6 +87,6 @@ calc_score_stats <- function(null_model, factor_matrix, model_type) {
 			est_cor[temp_col, temp_row] <- est_cor[temp_row, temp_col]
 		}
 	}
-	
-	return ( list(test_stats=test_stats, cor_mat=est_cor) )	
+
+	return ( list(test_stats=test_stats, cor_mat=est_cor) )
 }
